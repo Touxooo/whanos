9bb45d43ca2998c9a8e9f2a888091c6233835020
Touxooo <thomas.rabiet@epitech.eu>
2023-02-25 18:00:10 +0100

    [DEL] removed useless change

--- kubernetes/get_deployment.py
+++ kubernetes/get_deployment.py
@@ -33,7 +33,7 @@ class Whanos:
                         self.port.append(int(i))
     
     def add_ressources(self):
-        dico: dict = yaml.safe_load("""
+        dico: dict = yaml.safe_load("""resources:
             requests:
               cpu: 100m
               memory: 128Mi
